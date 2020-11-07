e64053ac1c32ab541f7b8b66a53725c42ed9713d
Pocol <project.asura@gmail.com>
2020-11-07 20:31:56 +0900

    IBL生成処理実装開始。

---
+++ res/shaders/IBLDiffuseCS.hlsl
@@ -0,0 +1,17 @@
+//-----------------------------------------------------------------------------
+// File : IBLDiffuseCS.hlsl
+// Desc : Generate Diffuse LD Map.
+// Copyright(c) Project Asura. All right reserved.
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#include "Math.hlsli"
+#include "BRDF.hlsli"
+
+
+[numthreads(1, 1, 1)]
+void main( uint3 DTid : SV_DispatchThreadID )
+{
+}
---
+++ res/shaders/IBLSpecularCS.hlsl
@@ -0,0 +1,16 @@
+//-----------------------------------------------------------------------------
+// File : IBLSpecularCS.hlsl
+// Desc : Generate Specular LD Map.
+// Copyright(c) Project Asura. All right reserved.
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#include "Math.hlsli"
+#include "BRDF.hlsli"
+
+[numthreads(1, 1, 1)]
+void main( uint3 DTid : SV_DispatchThreadID )
+{
+}
